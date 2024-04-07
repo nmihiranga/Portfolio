f8298d1a4eac545bc0bcf98426c23453d84b46f0
nmihiranga <mihiranganayana@gmail.com>
2024-04-07 12:01:47 +0530

    skills section added

--- my-app/context/active-section-context.tsx
+++ my-app/context/active-section-context.tsx
@@ -1,8 +1,6 @@
 'use client';
-import React, { createContext, useContext, useState } from 'react'
-import {links} from '@/lib/data';
-
-type SectionName = typeof links[number]['name'];
+import type { SectionName } from '@/lib/types';
+import React, { createContext, useContext, useState } from 'react';
 
 type ActiveSectionContextProviderProps = {
   children: React.ReactNode;
@@ -11,15 +9,18 @@ type ActiveSectionContextProviderProps = {
 type ActiveSectionContextType = {
   activeSection: SectionName;
   setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+  timeOfLastClick: number;
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null);
+export const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null);
 
 export default function ActiveSectionContextProvider({children}: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>('Home');
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
 
   return (
-    <ActiveSectionContext.Provider value={{activeSection, setActiveSection}}> {children} </ActiveSectionContext.Provider>
+    <ActiveSectionContext.Provider value={{activeSection, setActiveSection, timeOfLastClick, setTimeOfLastClick}}> {children} </ActiveSectionContext.Provider>
   );
 }
 
