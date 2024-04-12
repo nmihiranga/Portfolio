52b594e8ff03fdc56311840f0752e61b28b84450
nmihiranga <mihiranganayana@gmail.com>
2024-04-12 20:14:54 +0530

    contact me update

--- my-app/components/contact.tsx
+++ my-app/components/contact.tsx
@@ -3,7 +3,9 @@ import { sendEmail } from '@/actions/sendEmail';
 import { useSectionInView } from '@/lib/hooks';
 import { motion } from 'framer-motion'
 import React from 'react'
-import { FaPaperPlane } from 'react-icons/fa'
+import SubmitBtn from './submit-btn';
+import toast from 'react-hot-toast';
+
 
 export default function Contact() {
   const {ref} = useSectionInView('Contact', 0.9);
@@ -22,7 +24,14 @@ export default function Contact() {
 
       <form className='mt-10 flex flex-col' 
         action={async (formData) => {
-          await sendEmail(formData);
+          const {data, error} = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success('Email sent successfully!');
         }}
       >
         <input 
@@ -36,14 +45,12 @@ export default function Contact() {
         <textarea 
           required 
           name='message' 
-          maxLength={500} 
+          maxLength={5000} 
           placeholder='Your message' 
           className='h-52 my-3 rounded-lg border border-black/10 p-4' 
         />
 
-        <button type='submit' className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950'>
-          Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1' />
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   )
