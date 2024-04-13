5c88f713ed3de2da595d959c1c15be889bebb9ec
nmihiranga <mihiranganayana@gmail.com>
2024-04-14 00:35:12 +0530

    theme switch update

--- my-app/lib/data.ts
+++ my-app/lib/data.ts
@@ -1,7 +1,3 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
 import recipeapp from "@/public/recipeapp.png";
 import magneto from "@/public/magneto.png";
 import ytclone from "@/public/ytclone.png";
@@ -33,33 +29,6 @@ export const links = [
   },
 ] as const;
 
-export const experiencesData = [
-  {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
-    description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
-  },
-] as const;
-
 export const projectsData = [
   {
     title: "Full Stack Recipe app",
@@ -85,23 +54,24 @@ export const projectsData = [
 ] as const;
 
 export const skillsData = [
+  "Java",
+  "C++",
+  "Selenium",
+  "TestNG",
+  "Test Automation",
+  "Test Cases",
   "HTML",
   "CSS",
+  "Tailwind",
   "JavaScript",
   "TypeScript",
   "React",
   "Next.js",
   "Node.js",
-  "Git",
-  "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
+  "Google Cloud",
+  "AWS",
+  "MySQL",
   "PostgreSQL",
-  "Python",
-  "Django",
-  "Framer Motion",
+  "PHP",
+  "Git",
 ] as const;
