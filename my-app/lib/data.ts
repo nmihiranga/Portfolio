fc6f3959dd326af810932380c0025891be6b6f07
nmihiranga <mihiranganayana@gmail.com>
2024-05-15 04:33:04 +0530

    Projects update

--- my-app/lib/data.ts
+++ my-app/lib/data.ts
@@ -2,6 +2,9 @@ import recipeapp from "@/public/recipeapp.png";
 import magneto from "@/public/magneto.png";
 import ytclone from "@/public/ytclone.png";
 import seleniumCucumber from "@/public/SeleniumCucumber.png";
+import seleniumPython from "@/public/seleniumPython.png";
+import api from "@/public/api.png";
+import seleniumBehave from "@/public/seleniumBehave.png";
 
 export const links = [
   {
@@ -32,15 +35,47 @@ export const links = [
 
 export const projectsData = [
   {
-    title: "Cucumber with Selenium ",
+    title: "Selenium Behave BDD",
     description:
-      "A MAVEN project on setting up the Cucumber framework with Selenium in a Page Object Model concept along with the Web driver manager library.",
-    tags: ["Selenium", "Cucumber", "Java", "Test Automation"],
+      "Selenium Python project consists three main features with ten test scenarios. Used Behave BDD framework with Page Object Model along with Data Driven Test.",
+    tags: ["Selenium", "Python", "Behave", "POM", "Allure Report"],
+    imageUrl: seleniumBehave,
+    link: "https://github.com/nmihiranga/Selenium-BehaveBDD-project"
+  },
+  {
+    title: "API Test Automation",
+    description:
+      "Built using Java, Maven with Cucumber BDD framework. Automated four Features with seven Scenarios. Used Postman for initial manual testing.",
+    tags: ["Java", "Maven", "Cucumber", "Postman"],
+    imageUrl: api,
+    link: "https://github.com/nmihiranga/API-Test-Automation-Project"
+  },
+  {
+    title: "Selenium Python",
+    description:
+      "This project streamlines the process of finding accommodations on Booking.com by Automating the process using Selenium with Python.",
+    tags: ["Selenium", "Test Cases", "Python", "Test Automation"],
+    imageUrl: seleniumPython,
+    link: "https://github.com/nmihiranga/SeleniumPython_Project"
+  },
+  {
+    title: "Selenium Cucumber",
+    description:
+      "A Maven project on setting up the Cucumber BDD framework with Selenium in a Page Object Model concept along with the Web driver manager library.",
+    tags: ["Selenium", "Cucumber", "Maven", "Java", "POM"],
     imageUrl: seleniumCucumber,
     link: "https://github.com/nmihiranga/Cucumber-with-selenium-demo"
   },
   {
-    title: "Full Stack Recipe app",
+    title: "Selenium TestNG",
+    description:
+      "Tested Account creation, Login, Search products, Filter products, Add to cart, and Payouts using Selenium with TestNG.",
+    tags: ["Selenium", "TestNG", "Java", "Test cases", "Test automation"],
+    imageUrl: magneto,
+    link: "https://github.com/nmihiranga/Selenium-TestNG-Project"
+  },
+  {
+    title: "Full Stack Recipe App",
     description:
       "Recipe app that can search, view, add, and manage favorite recipes. I have used React for the front end, Node.js with Prisma for the back end, and PostgreSQL database.",
     tags: ["Node js", "React js", "PostgreSQL", "Typescript"],
@@ -48,15 +83,7 @@ export const projectsData = [
     link: "https://github.com/nmihiranga/Recipe-App"
   },
   {
-    title: "Magneto Online Store demo",
-    description:
-      "Tested Account creation, Login, Search products, Filter products, Add to cart, and Payouts using Selenium with TestNG.",
-    tags: ["Java", "TestNG", "Selenium", "Test cases", "Test automation"],
-    imageUrl: magneto,
-    link: "https://github.com/nmihiranga/Luma-eCommerce"
-  },
-  {
-    title: "YouTube clone",
+    title: "YouTube Clone",
     description:
       "A simple YouTube clone built using HTML and CSS. This is not a fully functional YouTube clone.",
     tags: ["HTML", "CSS"],
@@ -67,25 +94,29 @@ export const projectsData = [
 
 export const skillsData = [
   "Java",
+  "Python",
   "C++",
+  "AWS",
+  "GCP",
   "Selenium",
-  "TestNG",
   "Cucumber",
-  "Test Automation",
-  "Test Cases",
+  "Behave",
+  "TestNG",
+  "Postman",
+  "POM",
+  "Allure",
+  "Maven",
   "HTML",
   "CSS",
-  "Tailwind",
   "JavaScript",
   "TypeScript",
+  "PHP",
+  "Tailwind",
   "React",
   "Next.js",
   "Node.js",
-  "Google Cloud",
-  "AWS",
   "MySQL",
   "PostgreSQL",
-  "PHP",
   "Git",
   "GitHub",
 ] as const;
