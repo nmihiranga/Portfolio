10211de6bb320cdc3ad466fd4b63547b51b5a657
nmihiranga <mihiranganayana@gmail.com>
2024-08-07 00:46:43 +0530

    Resume Update

--- my-app/components/intro.tsx
+++ my-app/components/intro.tsx
@@ -17,7 +17,7 @@ export default function Intro() {
         <div className='relative'>
 
           <motion.div initial={{opacity: 0, scale: 0}} animate={{opacity: 1, scale: 1}} transition={{type: "tween", duration: 0.2}}>
-            <Image src='/profile-pic.jpg' alt={'img'} width='192' height='192' quality='95' priority={true} className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'/>
+            <Image src='/profile_pic.jpg' alt={'img'} width='192' height='192' quality='95' priority={true} className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'/>
           </motion.div>
 
           <motion.span className='absolute bottom-0 right-0 text-4xl' initial={{opacity: 0, scale: 0}} animate={{opacity: 1, scale: 1}} transition={{type: "spring", stiffness: 125, delay: 0.1, duration: 0.7}}>👋</motion.span>
